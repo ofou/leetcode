acbe01675fbdce81e86d778368a9ded426c10158
Omar Olivares <omar@olivares.cl>
2022-04-30 05:26:54 -0400

    ups

--- python/1480.running-sum-of-1-d-array.py
+++ python/1480.running-sum-of-1-d-array.py
@@ -12,7 +12,7 @@ class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         temp = []
         for i, n in enumerate(nums, start=1):
-            suma = functools.reduce(lambda a, b: a+b, nums[0:i], 0)
+            suma = reduce(lambda a, b: a+b, nums[0:i], 0)
             temp.append(suma)
         return temp
 
